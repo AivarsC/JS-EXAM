402ae94e5520c9a2017212ec6d6c632a020f8cfe
AivarsC <aivaryz@gmail.com>
2022-05-14 19:48:11 +0300

    Astuntos uzduoties sprendimas

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,42 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+// Sprendimas //
+
+function Calculator (a, b) {
+    this.a = a;
+    this.b = b;
+    this.sum = function () {
+       sum=`${this.a} + ${this.b} = ${this.a + this.b}`;
+       console.log(sum);
+       return sum;
+    };
+    this.substraction = function () {
+        substraction = `${this.a} - ${this.b} = ${this.a - this.b}`;
+        console.log(substraction);
+        return substraction;
+    };
+    this.multiplication = function () {
+        multiplication = `${this.a} x ${this.b} = ${this.a * this.b}`;
+        console.log(multiplication);
+        return multiplication;
+    };
+    this.division = function () {
+        if(this.b != 0){
+            division = `${this.a} / ${this.b} = ${this.a / this.b}`;
+            console.log(division);
+            return division;
+        }
+        else{
+            return console.log("Dalyba iš nulio negalima");
+        }     
+    };
+};
+
+var numbers = new Calculator(10, 3.3);
+
+numbers.sum();
+numbers.substraction();
+numbers.multiplication();
+numbers.division();
