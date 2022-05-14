0172f3da7c285c70747dfacca2474a5ffdeb54fa
AivarsC <aivaryz@gmail.com>
2022-05-14 18:11:23 +0300

    Penktos užduoties sprendimas

--- Task 5/script.js
+++ Task 5/script.js
@@ -19,3 +19,33 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30, hasDog: false },
   { id: '9', name: 'Daniel Cane', age: 51, hasDog: true },
 ];
+
+
+// Sprendimas //
+// 1. Grąžina į konsolę naudotojų, kurie turi šunį, vardus ir masyvą su šunų savininkais
+
+function filterDogOwers(users) {
+  console.log('Turi augintinį:');
+  const dogOwners = users.filter((user) => user.hasDog);
+  dogOwners_map = dogOwners.map((user) => user.name);
+  for (let i = 0; i <= dogOwners_map.length - 1; i++) {
+    console.log(dogOwners_map[i]); 
+  }
+  console.log(dogOwners_map);
+  return dogOwners_map;
+};
+
+filterDogOwers(users);
+
+// 2. Grąžina į konsolę naudotojų vardų masyvą, kurie yra pilnamečiai. Kadangi iki galo nesupratau, ką konkrečiai reikia grąžinti, tai grąžinu ir išfiltruotą pilną pirminį masyvą su pinlnamečių naudotojų vardais.
+
+function filterAdults(users) {
+  console.log('Yra pilnamečiai:');
+  const adults = users.filter((user) => user.age >= 18);
+  name_adults = adults.map((user) => user.name);
+  console.log(name_adults);
+  console.log(adults);
+  return name_adults;
+};
+
+filterAdults(users);
