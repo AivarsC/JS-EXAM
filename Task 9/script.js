86bd5611831f80bbaa009944551a6cc8f7c2029c
AivarsC <aivaryz@gmail.com>
2022-05-14 20:10:46 +0300

    Devintos uzduoties sprendimas

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,24 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+// Sprendimas //
+
+class Movie {
+	constructor(title, director, budget) {
+		this.title = title;
+		this.director = director;
+		this.budget = budget;
+		this.wasExpensive();
+	}
+	wasExpensive() {
+		const expensive = this.budget > 100000000;
+		return expensive;
+	}
+}
+
+const thisMovie = new Movie('Pirates of the Caribbean: On Stranger Tides', 'Rob Marshall', 422000000);
+
+console.log(thisMovie);
+console.log(`Was the movie expensive? `);
+console.log(thisMovie.wasExpensive());
